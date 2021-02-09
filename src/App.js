71e25855a78854ad3a8df5b735256cfb4867a84d
Tatiana Botia <tatianabb21@gmail.com>
2021-02-08 23:46:32 -0500

    header parte 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main className="content">
+        <section className="about">
+          <div>imagen</div>
+          <h3>The leader in interactive VR</h3>
+          <p>Founded in 2011, Loopstudios has been producing world-class virtual reality 
+              projects for some of the best companies around the globe. Our award-winning 
+              creations have transformed businesses through digital experiences that bind 
+              to their brand.
+          </p>
+        </section>
+        <section className="creations">
+          <h3>Our creations</h3>
+          <button>See all</button>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+          <div>imagen</div>
+        </section>
+      </main>
+      <footer>
+        <div>logo</div>
+        <ul>
+          <li>About</li>
+          <li>Careers</li>
+          <li>Events</li>
+          <li>Products</li>
+          <li>Support</li>
+        </ul>
+        <div>Redes sociales</div>
+        <span>© 2021 Loopstudios. All rights reserved.</span>
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </p>
+        <p>Coded by <a href="https://github.com/TatianaBotia">Tatiana Botia</a>.</p>
+
+
+      </footer>
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../images/logo.svg'
+import '../style/Header.css'
+
+const Header = () => {
+    
+    return(
+        <React.Fragment>
+           <header className="header">
+                <div className="header__navbar">
+                    <figure className="header__figure">
+                        <img src={logo} alt="Logo"/>
+                    </figure>
+                    <ul className="header__menu">
+                        <li className="header__menu-item">About</li>
+                        <li className="header__menu-item">Careers</li>
+                        <li className="header__menu-item">Events</li>
+                        <li className="header__menu-item">Products</li>
+                        <li className="header__menu-item">Support</li>
+                    </ul>
+                </div>
+                <div className="header__description">
+                <h2>Immersive experiences that deliver</h2>
+                </div>
+            </header>
+        </React.Fragment>
+    )
+}
+
+export default Header
